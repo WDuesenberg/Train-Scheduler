5b064a93b78d0f1230b195a4de377c79d393c695
Wyatt Duesenberg <wyatt.duesenberg@gmail.com>
2019-09-23 23:13:17 -0600

    updates

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -21,3 +21,67 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
+console.log(database);
+
+$("#add-train-btn").on("click", function(event) {
+    event.preventDefault();
+
+    var trainNum = $("#train-number-input").val().trim();
+    var destination = $("#destination-input").val().trim();
+    var firstTrain = moment($("#first-input").val().trim(), "MM/DD/YYYY").format("X");
+    var frequency = $("#frequency-input").val().trim();
+
+    var newTrain = {
+        train: trainNum,
+        destination: destination,
+        first: firstTrain,
+        frequency: frequency
+    };
+
+    database.ref().push(newTrain);
+
+    console.log(newTrain.train);
+    console.log(newTrain.destination);
+    console.log(newTrain.first);
+    console.log(newTrain.frequency);
+
+    alert("Train successfully added");
+
+    $("#train-number-input").val("");
+    $("#destination-input").val("");
+    $("first-input").val("");
+    $("frequency-input").val("");
+});
+
+database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
+
+    var trainNum = childSnapshot.val().train;
+    var destination = childSnapshot.val().destination;
+    var firstTrain = childSnapshot.val().first;
+    var frequency = childSnapshot.val().frequency;
+
+    console.log(trainNum);
+    console.log(destination);
+    console.log(firstTrain);
+    console.log(frequency);
+
+    var firstTrainPretty = moment.unix(firstTrain).format("MM/DD/YYYY");
+
+    var trainMonths = moment().diff(moment(firstTrain, "X"), "months");
+    console.log(trainMonths);
+
+    var newTrain = trainMonths * frequency;
+    console.log(newTrain);
+
+    var newRow = $('<tr>').append(
+        $("<td>").text(trainNum),
+        $("<td>").text(destination),
+        $("<td>").text(firstTrain),
+        $("<td>").text(frequency),
+        $("<td>").text(minutesAway),
+        // $("<td>").text(),
+    );
+
+    $("#train-data > tbody").append(newRow);
+});
